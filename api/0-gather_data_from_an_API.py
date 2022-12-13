2ad602f4e108ae137fffdded4568bfda8cd6ff09
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-12 20:16:40 -0500

    Try to arrange the docummented

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,30 +11,6 @@ def gather():
     This methos return the tasks of the users
     """
 
-    url_all = "https://jsonplaceholder.typicode.com/todos?userId={}"\
-        .format(argv[1])
-    url_user = "https://jsonplaceholder.typicode.com/users?id={}"\
-        .format(argv[1])
-
-    response_all = requests.get(url_all)
-    response_user = requests.get(url_user)
-
-    all_json = response_all.json()
-    user_json = response_user.json()
-    complete, task = 0, 0
-
-    for dates in all_json:
-        task += 1
-        if dates['completed'] is True:
-            complete += 1
-
-    name = user_json[0]['name']
-    print("Employee {} is done with tasks({}/{}):"\
-        .format(name, complete, task))
-    for dates in all_json:
-        if dates['completed'] is True:
-            print(f"\t{dates['title']}")
-
 
 if __name__ == '__main__':
     gather()
