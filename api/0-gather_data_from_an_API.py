fd7d36c42fd5ac0149c56b4491f5c81cd1b8f3bf
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-12 20:58:57 -0500

    Update the documment an function

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,10 +11,8 @@ def gather():
     This methos return the tasks of the users
     """
 
-    url_all = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        argv[1])
-    url_user = "https://jsonplaceholder.typicode.com/users?id={}".format(
-        argv[1])
+    url_all = f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}"
+    url_user = f"https://jsonplaceholder.typicode.com/users?id={argv[1]}"
 
     response_all = requests.get(url_all)
     response_user = requests.get(url_user)
@@ -23,7 +21,6 @@ def gather():
     user_json = response_user.json()
     comp, task = 0, 0
     list_task = []
-    
 
     for dates in all_json:
         task += 1
@@ -32,7 +29,7 @@ def gather():
             list_task.append(dates['title'])
 
     name = user_json[0]['name']
-    print("Employee {} is done with tasks({}/{}):".format(name, comp, task))
+    print(f"Employee {name} is done with tasks({comp}/{task}):")
     for task in list_task:
         print("\t{}".format(task))
 
